d5a0f166fbbad7042254eac8d1a582cfe6651cd2
NicolasRomero2165 <nicolas.romero.2165@gmail.com>
2026-03-30 20:30:49 -0300

    El link de IG se abre en una nueva pestaña

--- src/components/PieDePosteo.jsx
+++ src/components/PieDePosteo.jsx
@@ -1,7 +1,7 @@
 export const PieDePosteo = () => { 
     return (
     <div>
-        <footer><a href="https://www.instagram.com/disenospatata/">Instagram</a></footer>
+        <footer><a href="https://www.instagram.com/disenospatata/" target="_blank" >Instagram</a></footer>
     </div> 
     );
 };
